828970677e38800f54e503dd638296c6b9b3a66c
shagors <shajjadshagor@gmil.com>
2022-03-21 23:33:49 +0600

    navbar add

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h3>This is for products</h3>
+            </div>
+            <div className="cart-container">
+                <h4>Order summery</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
