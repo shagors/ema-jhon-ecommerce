dcf75cb34e350183ea57c5f07aa22f7749682bad
shagors <shajjadshagor@gmil.com>
2022-03-22 12:14:27 +0600

    add button and cart check

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,8 +5,17 @@ const Product = (props) => {
     const {name, img, seller, price, ratings} = props.product;
     return (
         <div className='product'>
-            <img src={img} alt="" />
-
+            <img src={img} alt=""/>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price : ${price}</p>
+                <p><small>Seller : {seller}</small></p>
+                <p><small>Ratings : {ratings}</small> stars</p>
+            </div>
+            <button onClick = {()=>props.handleAddToCart(props.product)}
+            className = 'btn-cart' >
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
