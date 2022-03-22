ba3c4f63d4abdf03fc5f261c911dde528a54c7f9
shagors <shajjadshagor@gmil.com>
2022-03-22 23:44:41 +0600

    order summery features add and price items add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({cart}) => {
+
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
     return (
-        <div>
+        <div className='cart'>
             <h4>Order summery</h4>
             <p>Selected Items : {cart.length}</p>
+            <p>Total price : ${total}</p>
+            <p>Total shipping : ${shipping}</p>
+            <p>Tax : ${tax}</p>
+            <h5>Grand Total : ${}</h5>
         </div>
     );
 };
