33f93887bc80c4fbce78d7fcf16920b9c6988e11
shagors <shajjadshagor@gmil.com>
2022-03-23 09:39:42 +0600

    order summery all fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,7 +9,8 @@ const Cart = ({cart}) => {
         total = total + product.price;
         shipping = shipping + product.shipping;
     }
-    const tax = (total * 0.1).toFixed(2);
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
         <div className='cart'>
             <h4>Order summery</h4>
@@ -17,7 +18,7 @@ const Cart = ({cart}) => {
             <p>Total price : ${total}</p>
             <p>Total shipping : ${shipping}</p>
             <p>Tax : ${tax}</p>
-            <h5>Grand Total : ${}</h5>
+            <h5>Grand Total : ${grandTotal}</h5>
         </div>
     );
 };
