66f192b97131c5459462aa86c75d1d97897571cc
shagors <shajjadshagor@gmail.com>
2022-04-10 14:48:18 +0600

    update some issues and fix shipment form

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -25,7 +25,8 @@ const Shipment = () => {
 
     const handleCreateUser = event => {
         event.preventDefault();
-        
+        const shipping = {name, email, address, phone};
+        console.log(shipping);
     }
     return (
         <div className='form-container'>
