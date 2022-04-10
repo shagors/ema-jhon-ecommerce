234787de900f5ef4cf28dbd7a72b68fd505f6fdc
shagors <shajjadshagor@gmail.com>
2022-04-10 14:30:59 +0600

    shiping page add and page lockup set up auto email get add

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import useCart from '../hooks/useCart';
@@ -10,6 +10,7 @@ import './Orders.css'
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+    const navigate = useNavigate();
 
     const handleRemoveProduct = product =>{
         const rest = cart.filter(pd => pd.id !== product.id);
@@ -25,9 +26,7 @@ const Orders = () => {
             </div>
             <div className="cart-container">
                 <Cart cart={cart}>
-                    <Link to='/inventory'>
-                        <button>Proceed Checkout</button>
-                    </Link>
+                        <button onClick={()=>navigate('/shipment')}>Proceed Shipment</button>
                 </Cart>
             </div>
         </div>
